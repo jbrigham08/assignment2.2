ec8e9c19d9ae459111fa9a7a4245f0d27c29de91
Software Engineer <SoftwareEngineer@MacBook-Air.attlocal.net>
2023-06-13 20:06:37 -0500

    assignment2.2

---
+++ index.js
@@ -0,0 +1,54 @@
+//Write JS code that solve the following problems
+//Q1. Using an if-else statement, determine if a number is divisible by 3, by 5, or both.
+
+
+//Add code here
+
+let q1test = 15;
+if (q1test % 3 === 0 && q1test % 5 === 0) {
+    console.log(`${q1test} is divisible by both 3 and 5.`);
+}
+else if (q1test % 3 === 0) {
+    console.log(`${q1test} is divisible by 3 only.`);
+}
+else if (q1test % 5 === 0) {
+    console.log(`${q1test} is divisible by 5 only.`);
+}
+else {
+    console.log(`${q1test} is not divisible by either 3 or 5.`);
+}
+
+//Q2. Using an if-else statement, determin is a year is a leap year or not.
+//Research what are the conditions to be considered a leap year.
+
+// A year is a leap year if the following conditions are satisfied:
+
+// The year is a multiple of 400.
+// The year is a multiple of 4 and not a multiple of 100.
+
+let year = 1999;
+// let year = 2000;
+// let year = 1900;
+// let year = 2024;
+
+//Add code here
+
+if (year % 4 === 0 && year % 100 !== 0) {
+    console.log(`${year} is a leap year.`);
+}
+
+else if (year % 100 === 0 && year % 400 === 0) {
+    console.log(`${year} is a leap year.`);
+} else {
+    console.log(`${year} is not a leap year.`);
+};
+
+//Q3. Use a loop to print all the numbers from 1 to 100 that are divisible by 3 or 4.
+
+//Add code here
+
+for (let i=1; i <= 100; i++) {
+    if (i % 3  === 0 || i % 4 === 0) {
+        console.log(i)
+    }
+}
